4bada3ad870121b72239f1be6532182b9843984c
mbetts7 <mbetts7@gmail.com>
2014-03-25 18:31:47 -0700

    setting up rails controller for entry creating

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,10 +3,16 @@ class EntriesController < ApplicationController
 
   def index
     entries = Entry.all.order(:created_at)
+    
     render json: entries.to_json
   end
 
   def create
+    entry_params = params[:entry].permit(:image_url, :link_url, :readability_title, 
+      :readability_author, :readability_domain, :readability_word_count, :readability_excerpt, :readability_content, :question, :user_id)
+    entry = Entry.create(entry_params)
+
+    render json: entry.to_json
   end
 
   def delete
