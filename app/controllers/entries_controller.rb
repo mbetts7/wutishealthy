02c6e8313d030680704ec173e9930179d6b74f54
mbetts7 <mbetts7@gmail.com>
2014-03-27 15:57:00 -0700

    slight updates

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,6 +14,13 @@ class EntriesController < ApplicationController
     render json: entry.to_json
   end
 
+  def update
+    # entry_params = params[:entry].permit(:image_url, :link_url, :readability_title, :readability_author, :readability_domain, :readability_word_count, :readability_excerpt, :readability_content, :readability_short_url, :question)
+    # entry = current_user.entries.create(entry_params)
+
+    render json: entry.to_json
+  end
+
   def delete
   end
 
